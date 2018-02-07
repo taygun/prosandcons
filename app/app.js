9a17b5f1316ba3c537a0bdebc7689e4b41892e7e
taygun <taygunagiali@gmail.com>
2018-02-07 21:29:29 +0200

    Added result directive

--- app/app.js
+++ app/app.js
@@ -30,12 +30,14 @@ prosAndConsApp.controller('prosAndConsController', function prosAndConsControlle
         $scope.reasons.push({text: $scope.reasonText, pro: $scope.reasonPro});
         $scope.reasonText = '';
         $scope.reasonPro = $scope.selectOptions[0].value;
+        $scope.resultText = getResultText();
     };
 
     $scope.removeReason = function(item) {
         var index = $scope.reasons.indexOf(item);
         $scope.reasons.splice(index, 1);
-    }
+        $scope.resultText = getResultText();
+    };
 
     $scope.filterPro = function (reason) {
         return reason.pro === true;
@@ -43,5 +45,39 @@ prosAndConsApp.controller('prosAndConsController', function prosAndConsControlle
 
     $scope.filterCon = function (reason) {
         return reason.pro !== true;
+    };
+
+    var emptyReasonsText = 'Start by adding pros and cons in the above input field!';
+    var proText = 'Pros outweigh the cons! You should decide in favor!';
+    var conText = 'Pros outweigh the cons! You should decide against the decision!';
+    var equalText = 'There seems to be a tie! Try to add more reasons!';
+
+    function getResultText() {
+        var resultText;
+        $scope.result = 'tie';
+        if ($scope.reasons < 1) {
+            resultText = emptyReasonsText;
+        } else {
+            var proCount = $scope.reasons.filter($scope.filterPro).length;
+            var conCount = $scope.reasons.length - proCount;
+
+            if (proCount > conCount) {
+                resultText = proText;
+                $scope.result = 'pro';
+            } else  if (proCount < conCount) {
+                resultText = conText;
+                $scope.result = 'con';
+            } else {
+                resultText = equalText;
+            }
+        }
+        return resultText;
+    };
+    $scope.resultText = getResultText();
+
+}).directive('choiceResult', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'result.html',
     };
 });
